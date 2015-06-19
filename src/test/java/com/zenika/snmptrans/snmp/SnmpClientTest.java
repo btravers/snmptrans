d6834a9812a94c18f8b30471340c2df903a22cad
btravers <btravers@insa-rennes.fr>
2015-06-19 14:29:52 +0200

    Change snmpclient tests

--- src/test/java/com/zenika/snmptrans/snmp/SnmpClientTest.java
+++ src/test/java/com/zenika/snmptrans/snmp/SnmpClientTest.java
@@ -1,43 +1,28 @@
 package com.zenika.snmptrans.snmp;
 
-import com.zenika.snmptrans.model.SnmpProcess;
-import com.zenika.snmptrans.model.Writer;
-import com.zenika.snmptrans.model.Result;
-import com.zenika.snmptrans.model.Server;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public abstract class SnmpClientTest {
 
     protected static final String sysDescr = "1.3.6.1.2.1.1.1.0";
 
     SnmpClient snmpClient = null;
-    SnmpProcess snmpProcess = null;
 
     @Test
     public void getAnSnmpVariable() throws IOException, InterruptedException {
-//        List<String> oids = new ArrayList<>();
-//        oids.add(sysDescr);
-//
-//        Writer writer = mock(Writer.class);
-//        List<Writer> writers = new ArrayList<>();
-//        writers.add(writer);
-//
-//        snmpClient.get(oids, snmpProcess, writers);
-//
-//        ArgumentCaptor<Collection> argumentCaptor = ArgumentCaptor.forClass(Collection.class);
-//
-//        verify(writer).doWrite(argumentCaptor.capture());
-//        assertThat(argumentCaptor.getValue().size()).isEqualTo(1);
-//        assertThat(((Result) argumentCaptor.getValue().iterator().next()).getAttr()).isEqualTo(sysDescr);
+        List<String> oids = new ArrayList<>();
+        oids.add(sysDescr);
+
+        Map<String, String> results = snmpClient.get(oids);
+
+        assertThat(results).containsKey(sysDescr);
+        assertThat(results.get(sysDescr)).isNotNull();
     }
 }
--- src/test/java/com/zenika/snmptrans/snmp/SnmpV2cClientTest.java
+++ src/test/java/com/zenika/snmptrans/snmp/SnmpV2cClientTest.java
@@ -1,31 +1,34 @@
 package com.zenika.snmptrans.snmp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zenika.snmptrans.model.SnmpProcess;
 import org.junit.After;
 import org.junit.Before;
 import org.snmp4j.smi.OID;
 
 import java.io.IOException;
 
-public class SnmpV2cClientTest /* extends SnmpClientTest */ {
+public class SnmpV2cClientTest extends SnmpClientTest {
 
-//    SnmpAgent agent;
-//
-//    @Before
-//    public void setup() throws IOException {
-//        this.agent = new SnmpAgent("udp:127.0.0.1/2001");
-//        this.agent.start();
-//        this.agent.unregisterManagedObject(agent.getSnmpv2MIB());
-//        this.agent.registerManagedObject(MOCreator.createReadOnly(new OID(sysDescr), "This Description is set By ShivaSoft"));
-//
-//        this.snmpClient = new SnmpV2cClient("udp:127.0.0.1/2001", "public");
-//        this.snmpClient.start();
-//
-//        // TODO create a an SnmpProcess file and load it
-//    }
-//
-//    @After
-//    public void tearDown() throws IOException {
-//        this.agent.stop();
-//        this.snmpClient.stop();
-//    }
+    SnmpAgent agent;
+
+    @Before
+    public void setup() throws IOException {
+        this.agent = new SnmpAgent("udp:127.0.0.1/2001");
+        this.agent.start();
+        this.agent.unregisterManagedObject(agent.getSnmpv2MIB());
+        this.agent.registerManagedObject(MOCreator.createReadOnly(new OID(sysDescr), "This Description is set By ShivaSoft"));
+
+        this.snmpClient = new SnmpV2cClient("udp:127.0.0.1/2001", "public");
+        this.snmpClient.start();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        //this.snmpProcess = objectMapper.readValue(this.getClass().getClassLoader().getResourceAsStream("conf.json"), SnmpProcess.class);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        this.agent.stop();
+        this.snmpClient.stop();
+    }
 }
